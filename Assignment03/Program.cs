87c55297a53bf2bae7c90e99cb063bc657f3b41a
Dell <Dell@ELMODASER>
2025-07-08 06:54:27 +0300

    Add project files.

---
+++ Assignment03/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment03
+{
+
+    class program   
+    { 
+        static void Main(string[]args)
+        {
+
+            #region Q1
+            Console.Write("Enter the value of n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($" Identity matrix of size {n} x {n}: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        Console.Write(" 1 ");
+                    else
+                        Console.Write(" 0 ");
+                }
+                Console.WriteLine();
+            }
+
+
+            #endregion
+
+            #region Q2
+
+            #endregion
+        }
+
+    }
+}
+
+
+           
